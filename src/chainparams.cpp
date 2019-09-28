bc89bad9821a9eb06d2beac1fbe6342588eb33f6
bitkanda <somuchllxx@outlook.com>
2019-09-27 19:49:10 -0700

    add default node

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -176,7 +176,7 @@ public:
         vSeeds.emplace_back("seed.bitcoin.sprovoost.nl");     // Sjors Provoost
         vSeeds.emplace_back("dnsseed.emzy.de");               // Stephan Oeste
 #endif
-
+        vSeeds.emplace_back("176.122.140.144");
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 0);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 5);
         base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 128);
