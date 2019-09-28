37de91c3f1aeb11776eb738601ce16a361647004
bitkanda <somuchllxx@outlook.com>
2019-09-28 09:38:23 -0700

    edit application default data of path

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -680,13 +680,13 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 
 fs::path GetDefaultDataDir()
 {
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Bitcoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Bitcoin
-    // Mac: ~/Library/Application Support/Bitcoin
-    // Unix: ~/.bitcoin
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\bitkanda
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\bitkanda
+    // Mac: ~/Library/Application Support/bitkanda
+    // Unix: ~/.bitkanda
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "bitkanda";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -696,10 +696,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/bitkanda";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".bitkanda";
 #endif
 #endif
 }
